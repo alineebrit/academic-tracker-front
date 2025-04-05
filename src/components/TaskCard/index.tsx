02fdf71ccac92f4df11d2a8218d11f071d8d1731
Luis Ricarte <luis.ricarte@synchro.com.br>
2025-04-05 02:59:01 -0300

    adiciona estilização nos cards

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -8,6 +8,14 @@ type TaskCardProps = {
 
 const TaskCard: React.FC<TaskCardProps> = ({task}) => {
     const ref = useRef<HTMLDivElement | null>(null);
+    const status = task.status;
+
+    const ColorStatus: {[key: string]: string} = {
+        NAO_INICIADA: "#ccc",
+        EM_PROCESSO: "#0000ff",
+        AGUARDANDO_CORRECAO: "#ff0000",
+        FINALIZADA: "#00ff00",
+    };
 
     const [{isDragging}, drag] = useDrag({
         type: "TASK",
@@ -27,10 +35,14 @@ const TaskCard: React.FC<TaskCardProps> = ({task}) => {
         <div
             ref={ref}
             className="kanban-task"
-            style={{opacity: isDragging ? 0.5 : 1}}
+            style={{
+                opacity: isDragging ? 0.5 : 1,
+                border: `3px solid ${ColorStatus[status]}`,
+            }}
         >
-            {task.title}
-            {task.description}
+            <h3>{task.title}</h3>
+            <br />
+            <p>{task.description}</p>
         </div>
     );
 };
